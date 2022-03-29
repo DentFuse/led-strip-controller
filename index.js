5947978df4484cda513e3f6fedbec93d8ee311ed
Yashasvi Dwivedi <DentFuse@users.noreply.github.com>
2022-03-29 14:34:58 +0530

    I got a bit carried away. Added two modes, one where you can choose the gradient, one automatic. Also previews gradients in terminal.

--- index.js
+++ index.js
@@ -2,22 +2,70 @@ const palette = require('image-palette');
 const pixels = require('image-pixels');
 const { prompt } = require('enquirer');
 const fs = require('fs');
+const gradient = require('gradient-string');
+const chalk = require('chalk');
 
 
+const str = '■'.repeat(48);
 
 async function main() {
-    const quantity = await prompt([{ type: 'select', 'name': 'file', message:'Input File?', choices: getAvailableFiles()}, { type: 'numeral', name: 'sets', message:'How many sets?', validate: notZero}, { type: 'numeral', name: 'colors', message:'How many colors per set?', validate: notZero}, { type: 'numeral', name: 'delay', message:'Delay between color fades? (ms)', validate: notZero}]);
+    const quantity = await prompt([{ type: 'select', 'name': 'file', message:'Input File?', choices: getAvailableFiles()}, { type: 'numeral', name: 'sets', message:'How many sets?', validate: notZero}, { type: 'numeral', name: 'colors', message:'How many colors per set?', validate: notZero}, { type: 'numeral', name: 'delay', message:'Delay between color fades? (ms)', validate: notZero}, { type: 'select', 'name': 'mode', message:'Which mode to run?', choices: ['Automatic', 'Manual']}]);
+    console.log('Generating colors, please wait...');
     let {ids, colors} = palette(await pixels('./' + quantity.file), quantity.sets * quantity.colors);
     colors = colors.map(x => [x[0], x[1], x[2]]);
-    let newColors = [];
-    for (let i = 0; i < quantity.sets; i++) {
-        newColors.push(colors.splice(0, quantity.colors));
+    console.log('Done!');
+    if(quantity.mode === 'Automatic') {
+        autoMode(quantity, colors);
+    } else if (quantity.mode === 'Manual') {
+        manualMode(quantity, colors);
     }
-    colors = newColors;
-    // console.log(colors);
+}
+
+async function manualMode(quantity, colors) {
+    console.log('System will select colors, you can select gradients');
+    const colorMap = {};
     for (let i = 0; i < colors.length; i++) {
-        const colorSet = colors[i];
-        const code = generateCode(colorSet, quantity.delay);
+        colorMap[i] = colors[i];
+    }
+    colorSets = [];
+    for (let i = 0; i < quantity.sets; i++) {
+        let colorSet = await prompt({type: 'multiselect', name: 'name', message: 'Colors to include in fade ' + i,  choices: colors.map(x => ({ name: x.toString() + chalk.rgb(x[0], x[1], x[2])(' ■'), value: x})), result(names) { return this.map(names) }});
+        colorSet = Object.values(colorSet.name);
+        let tmp = await prompt({ type: 'sort', name:'name', message: 'Sort the colors in your preference', numbered: true, hint: 'Use <shift>+<up>/<down> to sort', choices: colorSet.map(x => ({ name: x.toString() + chalk.rgb(x[0], x[1], x[2])(' ■'), value: x})), result(names) { return this.map(names) }})
+        colorSet = Object.values(tmp.name);
+        colorSets.push(colorSet);
+    }
+    printGradients(colorSets);
+    writeColors(colorSets, quantity.delay);
+}
+
+async function autoMode(quantity, colors) {
+    console.log('Automatic mode, system will generate gradients automatically, don\'t expect them to be too good.')
+    let colorSets = [];
+    for (let i = 0; i < quantity.sets; i++) {
+        colorSets.push(colors.splice(0, quantity.colors));
+    }
+    printGradients(colorSets);
+    writeColors(colorSets, quantity.delay);
+}
+
+function printGradients(colorSets) {
+    console.log("Generated gradients:\n");
+    for (let i = 0; i < colorSets.length; i++) {
+        const colorSet = colorSets[i];
+        const gradientColors = [];
+        for(let x = 0; x < colorSet.length; x++) {
+            color = colorSet[x];
+            gradientColors.push('#' + rgbToHex(color[0], color[1], color[2]))
+        }
+        console.log(i + '.txt: ' + gradient(gradientColors)(str));
+    }
+}
+
+function writeColors(colorSets, delay) {
+    for (let i = 0; i < colorSets.length; i++) {
+        const colorSet = colorSets[i];
+        const code = generateCode(colorSet, delay);
         fs.writeFileSync('./' + i + '.txt', code);
     }
     console.log('Wrote code successfully!');
@@ -41,6 +89,15 @@ function getAvailableFiles() {
     let files = fs.readdirSync('.');
     files = files.filter(x => x.endsWith('.jpg') || x.endsWith('.png'));
     return files;
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 main();
